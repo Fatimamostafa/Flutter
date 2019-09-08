256c6781b300cd22e8688f112fb602910d76db9f
Fatima Mostafa <the.fatimamostafa@gmail.com>
2019-09-08 23:57:03 +0600

    Button actions

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,40 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main() =>
+    runApp(MyApp()); //For only one expression method can be right be like this
 
-}
-
-class MyApp {
-
+class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Basics'),
+        ),
+        body: Column(
+          children: [
+            Text('The Question'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer Chosen!'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: ()  {
+                //..
+                print('Answer Chosen!');
+              }
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
