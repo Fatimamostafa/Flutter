1f8031096594cc70329d972cc8414e6079535719
Fatima Mostafa <the.fatimamostafa@gmail.com>
2019-10-09 19:14:56 +0600

    added category list

---
+++ lib/widgets/category_item.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  String title;
+  Color color;
+
+  CategoryItem(this.title, this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      child: Text(title, style: Theme.of(context).textTheme.title,),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            color.withOpacity(0.7),
+            color,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(15),
+      ),
+    );
+  }
+}
