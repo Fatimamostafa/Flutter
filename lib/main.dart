a38c7c7370a598ae798991296a59506e880d7c4d
Fatima Mostafa <the.fatimamostafa@gmail.com>
2019-11-04 12:17:39 +0600

    main updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(new MyApp());
+  });
 }
 
-class MyApp {
 
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          hintColor: Color(0xFFC0F0E8),
+          primaryColor: Color(0xFF80E1D1),
+          canvasColor: Colors.transparent),
+      //fontFamily: "Montserrat",
+      //home: Home(),
+    );
+  }
 }
