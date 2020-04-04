90dd11dffabe94c8e1d2b5b27b7ae210e5c30232
Fatima Mostafa <the.fatimamostafa@gmail.com>
2020-04-04 23:00:15 +0600

    Fetching orders from web

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -21,16 +21,40 @@ class OrderItem {
 
 class Orders with ChangeNotifier {
   List<OrderItem> _orders = [];
+  static const url = "https://flutter-udemy-c46b2.firebaseio.com/orders.json";
 
   List<OrderItem> get orders {
     return [..._orders];
   }
 
+  Future<void> fetchAndSetOrders() async {
+    final response = await http.get(url);
+    final List<OrderItem> loadedOrders = [];
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+
+    if(extractedData == null) return;
+
+    extractedData.forEach((orderId, orderData) {
+      loadedOrders.add(OrderItem(
+          id: orderId,
+          amount: orderData['amount'],
+          dateTime: DateTime.parse(orderData['dateTime']),
+          products: (orderData['products'] as List<dynamic>)
+              .map((item) => CartItem(
+                  id: item['id'],
+                  price: item['price'],
+                  quantity: item['quantity'],
+                  title: item['title']))
+              .toList()));
+    });
+    _orders = loadedOrders.reversed.toList();
+    notifyListeners();
+  }
+
   Future<void> addOrder(List<CartItem> cartProducts, double total) async {
     final timeStamp = DateTime.now();
-    const productUrl =
-        "https://flutter-udemy-c46b2.firebaseio.com/orders.json";
-    final response = await http.post(productUrl,
+
+    final response = await http.post(url,
         body: json.encode({
           'amount': total,
           'dateTime': timeStamp.toIso8601String(),
--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -91,6 +91,9 @@ class Products with ChangeNotifier {
     try {
       final response = await http.get(productUrl);
       final extractedData = json.decode(response.body) as Map<String, dynamic>;
+
+       if(extractedData == null) return;
+
       List<Product> loadedProducts = [];
       extractedData.forEach((prodId, prodData) {
         loadedProducts.add(Product(
